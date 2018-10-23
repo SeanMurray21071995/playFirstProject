eed1ab31f08480bd1b85f7f02d2e6dd8a877a798
seanmurray21071995 <sean.murray@academytrainee.com>
2018-10-23 09:01:02 +0100

    First play project commit

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,5 +8,14 @@ class Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
+  def second(num:Int) = Action {
+    Ok(views.html.secondPage("Second page woop woop"+num))
+  }
+  def secondStatic=Action{
+    Ok(views.html.secondPage("dffgdgsdsg"))
+  }
+  def secondOptional=Action{
+    Ok
+  }
 
 }
